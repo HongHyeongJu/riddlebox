e15b78cd3af2e40cbdc56fa31697bc0ffae90763
hhj <hhju0720@gmail.com>
2024-01-12 23:50:05 +0900

    Update 로그인 인터셉터 초기 코드

--- src/main/java/com/labmate/riddlebox/web/argumentresolver/LoginMemberArgumentResolver.java
+++ src/main/java/com/labmate/riddlebox/web/argumentresolver/LoginMemberArgumentResolver.java
@@ -1,2 +1,43 @@
-package com.labmate.riddlebox.web.argumentresolver;public class LoginMemberArgumentResolver {
+package com.labmate.riddlebox.web.argumentresolver;
+
+import com.labmate.riddlebox.entity.Member;
+import com.labmate.riddlebox.security.SessionConst;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Slf4j
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+
+        log.info("supportsParameter 실행");
+
+        boolean hasLoginAnnotation = parameter.hasParameterAnnotation(Login.class);
+        boolean hasMemberType = Member.class.isAssignableFrom(parameter.getNestedParameterType());
+
+        return hasLoginAnnotation && hasMemberType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) throws Exception {
+
+        log.info("resolveArgument 실행");
+
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        HttpSession session = request.getSession(false);
+        if(session==null){
+            return null;
+        }
+
+        return session.getAttribute(SessionConst.LOGIN_MEMBER);
+    }
 }
--- src/main/java/com/labmate/riddlebox/web/argumentresolver/WebConfig.java
+++ src/main/java/com/labmate/riddlebox/web/argumentresolver/WebConfig.java
@@ -1,2 +1,34 @@
-package com.labmate.riddlebox.web.argumentresolver;public class WebConfig {
+package com.labmate.riddlebox.web.argumentresolver;
+
+import com.labmate.riddlebox.web.interceptor.LogInterceptor;
+import com.labmate.riddlebox.web.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.util.List;
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**").excludePathPatterns("/css/**", "/*.ico", "/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/", "/members/add", "/login", "/logout",
+                        "/css/**", "/*.ico", "/error"
+                );
+
+    }
 }
