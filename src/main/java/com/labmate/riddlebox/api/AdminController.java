e2b6f9cbd05f6506563943c32a41b88479431b09
hhj <hhju0720@gmail.com>
2024-01-31 09:18:31 +0900

    Add 관리자 페이지 기본 레이아웃

--- src/main/java/com/labmate/riddlebox/api/AdminController.java
+++ src/main/java/com/labmate/riddlebox/api/AdminController.java
@@ -1,4 +1,31 @@
 package com.labmate.riddlebox.api;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("admin")
 public class AdminController {
+
+    @GetMapping("/notice/list") //공지사항 목록
+    public String showNoticeList(Model model) {
+        model.addAttribute("pageType", "noticeList");
+        model.addAttribute("title", "RiddleBox Notice");
+
+        return "layout/admin/admin_layout_base";
+    }
+
+
+    @GetMapping("/notice/new") //공지사항 작성
+    public String showNoticeDetail(Model model) {
+        model.addAttribute("pageType", "noticeDetail");
+        model.addAttribute("pageMode", "new");
+        model.addAttribute("title", "RiddleBox Notice");
+
+        return "layout/admin/admin_layout_base";
+    }
+
+
 }
